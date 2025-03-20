2283297a0248f8e96d0b1fa403ef85db08466c4d
erlonviana <erlonviana@hotmail.com>
2025-03-20 18:44:14 -0300

    exercicios

---
+++ Exercicios-Aula 0.3 -  Classe, data e moeda.js
@@ -0,0 +1,41 @@
+//Classes e objetos
+1 – Crie uma classe em JavaScript chamada Heroi, que
+represente um herói genérico. Essa classe deve conter os seguintes atributos:
+- nome
+- vida
+- velocidade
+- forca
+
+Além disso, implemente os seguintes métodos na classe Heroi:
+- correr(): Exibe uma mensagem informando que o
+herói está correndo.
+- andar(): Exibe uma mensagem informando que o
+herói está andando.
+- atacar(): Exibe uma mensagem informando que o
+herói está atacando.
+- defender(): Exibe uma mensagem informando que o
+herói está se defendendo.
+Use o “return” para retornar as mensages.
+
+Após criar a classe Heroi, instancie três objetos baseados nela, representando os heróis. Atribua valores para os atributos que foram definidos na classe para cada herói, inserindo também os atributos e métodos adicionais, conforme abaixo:
+Homem-Aranha
+Atributo adicional: 
+- teia (0 ou 1, indicando se ele pode ou não soltar teia)
+Método adicional:
+- sentidoAranha(): Exibe uma mensagem informando que ele
+detectou perigo.
+ 
+Superman
+Atributo adicional: 
+- podeVoar (0 ou 1, indicando se ele pode ou não voar)
+Método adicional:
+- visaoCalor(): Exibe uma mensagem informando que ele está
+usando sua visão de calor.
+
+Batman
+Atributo adicional: 
+- esconder (0 ou 1, indicando se ele está se escondendo ou
+não)
+Método adicional:
+- investigar(): Exibe uma mensagem informando que ele está
+investigando um crime.
